5606de0a884ab346c79640d7702cbbf3fd5df3d8
siswanto@live.dk <Hfg93rdqhfg>
2021-10-11 15:06:41 +0200

    Pizza and toppings constructed

--- src/PizzaMenu.java
+++ src/PizzaMenu.java
@@ -1,2 +1,23 @@
+// Jimmy start
 public class PizzaMenu {
+    private int pizzaId;
+    private String name;
+    private int price;
+
+    public PizzaMenu(int pizzaId, String name, int price) {
+        this.pizzaId = pizzaId;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "PizzaMenu{" +
+                "pizzaId=" + pizzaId +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
+
+//Jimmy slut
--- src/PizzaTopping.java
+++ src/PizzaTopping.java
@@ -1,2 +1,3 @@
 public class PizzaTopping {
+    private String toppingNames;
 }
