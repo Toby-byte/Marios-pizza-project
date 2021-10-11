b37fa22c4bbb126c2bdfe8d793c2d3724c2ad4dd
Toby-byte <tobiasbogenielsen@gmail.com>
2021-10-11 15:39:56 +0200

    Class Structure v.1

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please input pizza id:");
+        String pizzaId = scanner.nextLine();
+        int newPizzaId = Integer.parseInt(pizzaId);
+        System.out.println("please input pizza price:");
+        String pizzaPrice = scanner.nextLine();
+        int newPizzaPrice = Integer.parseInt(pizzaPrice);
+        System.out.println("please input pizza name:");
+        String name = scanner.nextLine();
+
+        Orders pizza = new Orders(newPizzaId,newPizzaPrice,name);
+
+        System.out.println(pizza);
+    }
 }
--- src/Test_class.java
+++ src/Test_class.java
@@ -1,6 +1,20 @@
 public class Test_class {
+String nameId;
+    String topping1;
+    String topping2;
+    String topping3;
+    String topping4;
+
+    public Test_class(String nameId, String topping1, String topping2, String topping3, String topping4) {
+        this.nameId = nameId;
+        this.topping1 = topping1;
+        this.topping2 = topping2;
+        this.topping3 = topping3;
+        this.topping4 = topping4;
+    }
+
     public static void main(String[] args) {
-        Orders asd = new Orders(1,69,"visuvio");
-        System.out.println(asd);
+        Test_class v1 = new Test_class("1","tomato","cheese","skinke","oregano");
+        Test_class v2 = new Test_class("3","tomato","cheese", "jkfdj","fjgkf");
     }
 }
