594b18fbd6c22b581227f09e979907952e3447d0
Toby-byte <tobiasbogenielsen@gmail.com>
2021-10-12 11:39:29 +0200

    Mario menu done!

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        PizzaMenu p1 = new PizzaMenu("Vesuvio","tomato","cheese","skinke","oregano");
-        PizzaMenu p2 = new PizzaMenu("Amerikaner","tomato","cheese", "oksefars","oregano");
-        PizzaMenu p3 = new PizzaMenu("Cacciatore","tomato","cheese", "pepperoni","oregano");
-        PizzaMenu p4 = new PizzaMenu("Bertil","tomato","cheese", "bacon","oregano");
+
     }
 }
