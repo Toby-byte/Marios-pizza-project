b1e63aa0c460f37f5cafb8e8beb8970e0a2478af
Toby-byte <tobiasbogenielsen@gmail.com>
2021-10-12 10:43:55 +0200

    Added 4 pizzas to main

--- src/PizzaMenu.java
+++ src/PizzaMenu.java
@@ -1,5 +1,5 @@
 public class PizzaMenu {
-    private String name;
+    private static String name;
     private String topping1;
     private String topping2;
     private String topping3;
@@ -11,25 +11,5 @@ public class PizzaMenu {
         this.topping2 = topping2;
         this.topping3 = topping3;
         this.topping4 = topping4;
-    }
-
-    PizzaMenu p1 = new PizzaMenu("Vesuvio","tomato","cheese","skinke","oregano");
-    PizzaMenu p2 = new PizzaMenu("Amerikaner","tomato","cheese", "oksefars","oregano");
-    PizzaMenu p3 = new PizzaMenu("Cacciatore","tomato","cheese", "pepperoni","oregano");
-    PizzaMenu p4 = new PizzaMenu("Bertil","tomato","cheese", "bacon","oregano");
-
-    @Override
-    public String toString() {
-        return "PizzaMenu{" +
-                "name='" + name + '\'' +
-                ", topping1='" + topping1 + '\'' +
-                ", topping2='" + topping2 + '\'' +
-                ", topping3='" + topping3 + '\'' +
-                ", topping4='" + topping4 + '\'' +
-                ", p1=" + p1 +
-                ", p2=" + p2 +
-                ", p3=" + p3 +
-                ", p4=" + p4 +
-                '}';
     }
 }
