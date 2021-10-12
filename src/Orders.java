480b7ae7dc97b46c6e748897aa467bd7f754bc4c
siswanto@live.dk <Hfg93rdqhfg>
2021-10-12 09:33:17 +0200

    Orders setOrderStatus

--- src/Orders.java
+++ src/Orders.java
@@ -1,31 +1,44 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class Orders {
-    int pizzaId;
-    int pizzaPrice;
-    String name;
+    private int orderId;
+    private String costumerName;
+    private int[] pizzaIdArray;
+    private String timeStamp;
+    private boolean orderStatus;
 
-    public void pizzaTimestamp() {
+    public String pizzaTimestamp() {
+        String timeStamp;
         //https://www.javatpoint.com/java-get-current-date
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
-        System.out.println(dtf.format(now));
+        timeStamp = dtf.format(now);
+        return timeStamp;
     }
 
-    public Orders(int pizzaId, int pizzaPrice, String name) {
-        this.pizzaId = pizzaId;
-        this.pizzaPrice = pizzaPrice;
-        this.name = name;
-        pizzaTimestamp();
+    public Orders(int orderId, String costumerName, int[] pizzaIdArray, String timeStamp, boolean orderStatus) {
+        this.orderId = orderId;
+        this.costumerName = costumerName;
+        this.pizzaIdArray = pizzaIdArray;
+        this.timeStamp = timeStamp;
+        this.orderStatus = orderStatus;
+    }
+
+    public void setOrderStatus(boolean orderStatus) {
+        this.orderStatus = orderStatus;
+        orderStatus = false;
     }
 
     @Override
     public String toString() {
         return "Orders{" +
-                "pizzaId=" + pizzaId +
-                ", pizzaPrice=" + pizzaPrice +
-                ", name='" + name + '\'' +
+                "orderId=" + orderId +
+                ", costumerName='" + costumerName + '\'' +
+                ", pizzaIdArray=" + Arrays.toString(pizzaIdArray) +
+                ", timeStamp='" + timeStamp + '\'' +
+                ", orderStatus=" + orderStatus +
                 '}';
     }
 }
